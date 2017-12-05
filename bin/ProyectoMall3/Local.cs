4283b11c7c5a91234b964073da66d827aab3c6fe
Jiibanez <31524545+Jiibanez@users.noreply.github.com>
2017-12-05 12:27:22 -0300

    Add files via upload

---
+++ bin/ProyectoMall3/Local.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMall3
+{
+    class Local
+    {
+        public string nombre, categoria;
+        public int numEmpleados, areaLocal, precioMin, precioMax, clientesDiaAnterior, clientesTotales, gananciasDiaAnterior, gananciasTotales, promedioSueldos;
+
+        public Local(string nombre, string categoria, int numEmpleados, int areaLocal, int precioMin, int precioMax, int promedioSueldos)
+        {
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.numEmpleados = numEmpleados;
+            this.areaLocal = areaLocal;
+            this.precioMin = precioMin;
+            this.precioMax = precioMax;
+            this.promedioSueldos = promedioSueldos;
+            clientesDiaAnterior = 0;
+            clientesTotales = 0;
+            gananciasDiaAnterior = 0;
+            gananciasTotales = 0;
+        }
+
+        public override string ToString()
+        {
+            return this.nombre;
+        }
+    }
+}
+
---
+++ bin/ProyectoMall3/Piso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoMall3
+{
+    class Piso
+    {
+        public int altura;
+        public int area;
+        public List<Local> listaLocales = new List<Local>();
+
+        public Piso(int altura, int area, List<Local> listaLocales)
+        {
+            this.altura = altura;
+            this.area = area;
+            this.listaLocales = listaLocales;
+        }
+
+        public override string ToString()
+        {
+            return "Piso" + this.altura;
+        }
+    }
+}
